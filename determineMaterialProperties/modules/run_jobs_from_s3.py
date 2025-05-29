3a1ed1be1ae3d6754bb32dd4734e4a689a311dee
ryantheengineer <ryan.larson.wa@gmail.com>
2025-05-29 10:36:11 -0600

    Create run_jobs_from_s3.py
    Initial pass at user script for getting S3 job data and running optimization

---
+++ determineMaterialProperties/modules/run_jobs_from_s3.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 28 14:51:39 2025
+
+@author: Ryan.Larson
+"""
+
+import boto3
+import yaml
+import os
+import subprocess
+
+s3 = boto3.client('s3')
+
+with open('s3_config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+    
+bucket = config['bucket']
+prefix = config['prefix']
+
+def list_jobs():
+    result = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    return [obj['Key'] for obj in result.get('Contents', []) if obj['Key'].endswith('.yaml')]
+
+def claim_job(job_key):
+    # Implement a claim system (e.g., upload 'in_progress' marker)
+    try:
+        lock_key = job_key.replace('.yaml', '.lock')
+        s3.head_object(Bucket=bucket, Key=lock_key)
+        return False  # Already claimed
+    except:
+        s3.put_object(Bucket=bucket, Key=lock_key, Body=b'claimed')
+        return True
+
+def process_job(job_key):
+    # Download YAML and related files
+    job_folder = os.path.dirname(job_key)
+    local_job_dir = '/tmp/job'
+    
+    # Clean local job directory
+    if os.path.exists(local_job_dir):
+        for f in os.listdir(local_job_dir):
+            os.remove(os.path.join(local_job_dir, f))
+    else:
+        os.makedirs(local_job_dir)
+        
+    # Download all files in the job folder
+    result = s3.list_objects_v2(Bucket=bucket, Prefix=job_folder + '/')
+    for obj in result.get('Contents', []):
+        key = obj['Key']
+        filename = os.path.basename(key)
+        local_path = os.path.join(local_job_dir, filename)
+        s3.download_file(bucket, key, local_path)
+    
+    # Run FEA + optimization
+    job_yaml_path = os.path.join(local_job_dir, os.path.basename(job_key))
+    result = subprocess.run(
+        ['python', 'runOptimizationCLI.py', job_yaml_path],
+        cwd=local_job_dir,
+        capture_output=True,
+        text=True
+    )
+    
+    # # Optionally save stdout/stderr to files
+    # with open(os.path.join(local_job_dir, 'stdout.log'), 'w') as f:
+    #     f.write(result.stdout)
+    # with open(os.path.join(local_job_dir, 'stderr.log'), 'w') as f:
+    #     f.write(result.stderr)
+    
+    # Upload results back to the job folder
+    for fname in os.listdir(local_job_dir):
+        if fname.endswith('.txt') or fname.endswith('.log') or fname.startswith('results'):
+            s3.upload_file(
+                os.path.join(local_job_dir, fname),
+                bucket,
+                f'{job_folder}/{fname}'
+            )
+    
+
+for job_key in list_jobs():
+    if claim_job(job_key):
+        process_job(job_key)
+        break  # Stop after one job; instance shuts down or can loop
